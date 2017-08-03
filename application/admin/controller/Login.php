6f387eaa5927170864fd5b893410c5f8ce6e6c55
lhypoi <904579880@qq.com>
2017-08-03 09:09:31 +0800

    first commit

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+/**
+* 登陆控制器
+*/
+class Login extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+
+    public function check_verify(){
+        echo 1;
+    }
+}
+ ?>
